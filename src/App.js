2bff8f7fd4a2ec2e95a67d57b96e1f105936654a
Masahiro Mark Tsujimura <masahiromarktsujimura@gmail.com>
2020-03-28 17:07:48 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Main className="App">
+            <h1>React Hook Form</h1>
+            <form>
+                <label>Email</label>
+                <input />
+            </form>
+        </Main>
+    );
 }
+
+const Main = styled.main`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    label {
+        display: block;
+    }
+`;
+
+
 
 export default App;
